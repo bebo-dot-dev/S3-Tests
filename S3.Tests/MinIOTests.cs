7adcff0ae596f93fdd51e20fb93f10575aeca76b
Joe Lippa <joe@bebo.dev>
2023-07-31 19:42:02 +0100

    feat: added another MinIO test case

--- S3.Tests/MinIOTests.cs
+++ S3.Tests/MinIOTests.cs
@@ -11,34 +11,36 @@ namespace S3.Tests
     {
         private const string MinioUsername = "minio_test";
         private const string MinioPassword = "minio_test";
-        private const string MinioImage = "minio/minio:RELEASE.2023-07-21T21-12-44Z"; //MinIO 21/07/2023 latest
-        private MinioContainer _minioContainer = null!;
-            
+        private const string LatestMinioImage = "minio/minio:RELEASE.2023-07-21T21-12-44Z"; //MinIO 21/07/2023 latest
+        private const string LatestMinioImageErrorMessage = "The request signature we calculated does not match the signature you provided. Check your key and signing method.";
+        private const string OlderMinioImage = "minio/minio:RELEASE.2023-04-13T03-08-07Z.fips"; //MinIO 13/04/2023 older
+        private const string OlderMinioImageErrorMessage = "The provided 'x-amz-content-sha256' header does not match what was computed.";
+        
         [SetUp]
-        public async Task SetUp()
+        public void SetUp()
         {
             Environment.SetEnvironmentVariable("MINIO_ROOT_USER", MinioUsername);
             Environment.SetEnvironmentVariable("MINIO_ROOT_PASSWORD", MinioPassword);
-            
-            _minioContainer = new MinioBuilder()
-                .WithImage(MinioImage)
+        }
+        
+        [Test]
+        public async Task PutObjectAsync_WhenUsingLatestMinIOWithChecksumAlgorithmSHA256_ExpectFailure()
+        {
+            //arrange
+            var latestMinio = new MinioBuilder()
+                .WithImage(LatestMinioImage)
                 .WithUsername(MinioUsername)
                 .WithPassword(MinioPassword)
                 .Build();
 
-            await _minioContainer.StartAsync();
-        }
-        
-        [Test]
-        public async Task PutObjectAsync_WhenUsingMinIOWithChecksumAlgorithmSHA256_ExpectFailure()
-        {
-            //arrange
+            await latestMinio.StartAsync();
+            
             var client = new AmazonS3Client(
-                new BasicAWSCredentials(_minioContainer.GetAccessKey(), _minioContainer.GetSecretKey()),
+                new BasicAWSCredentials(latestMinio.GetAccessKey(), latestMinio.GetSecretKey()),
                 new AmazonS3Config
                 {
                     UseHttp = true, //plain old http
-                    ServiceURL = _minioContainer.GetConnectionString(), //target the request at the test container MinIO instance
+                    ServiceURL = latestMinio.GetConnectionString(), //target the request at the test container MinIO instance
                     ForcePathStyle = true, //using path style addressing for MinIO
                     ProxyHost = "127.0.0.1", //route through local proxy to capture the request/response
                     ProxyPort = 8866
@@ -60,7 +62,49 @@ namespace S3.Tests
             //assert
             await act.Should()
                 .ThrowAsync<AmazonS3Exception>()
-                .WithMessage("The request signature we calculated does not match the signature you provided. Check your key and signing method.");
+                .WithMessage(LatestMinioImageErrorMessage);
+        }
+        
+        [Test]
+        public async Task PutObjectAsync_WhenUsingOlderMinIOWithChecksumAlgorithmSHA256_ExpectFailure()
+        {
+            //arrange
+            var olderMinio = new MinioBuilder()
+                .WithImage(OlderMinioImage)
+                .WithUsername(MinioUsername)
+                .WithPassword(MinioPassword)
+                .Build();
+
+            await olderMinio.StartAsync();
+            
+            var client = new AmazonS3Client(
+                new BasicAWSCredentials(olderMinio.GetAccessKey(), olderMinio.GetSecretKey()),
+                new AmazonS3Config
+                {
+                    UseHttp = true, //plain old http
+                    ServiceURL = olderMinio.GetConnectionString(), //target the request at the test container MinIO instance
+                    ForcePathStyle = true, //using path style addressing for MinIO
+                    ProxyHost = "127.0.0.1", //route through local proxy to capture the request/response
+                    ProxyPort = 8866
+                });
+            await client.PutBucketAsync(BucketName);
+            
+            var request = new PutObjectRequest
+            {
+                BucketName = BucketName,
+                Key = FileResourceName,
+                InputStream = FileResourceStream,
+                ContentType = "text/plain",
+                ChecksumAlgorithm = ChecksumAlgorithm.SHA256
+            };
+
+            //act
+            Func<Task> act = async () => await client.PutObjectAsync(request);
+            
+            //assert
+            await act.Should()
+                .ThrowAsync<AmazonS3Exception>()
+                .WithMessage(OlderMinioImageErrorMessage);
         }
     }
 }
